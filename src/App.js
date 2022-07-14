0b37163229d236dfeffc031551eed50cb5acd87a
KeiferRamos <keiferramos2001@gmail.com>
2022-07-14 10:34:07 +0800

    gg

--- src/App.js
+++ src/App.js
@@ -2,8 +2,16 @@ import Sidebar from "./components/sidebar/sidebar";
 import RoutesPage from "./routes/routes";
 import "./styles/app.css";
 import Navbar from "./components/navbar/navbar";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 function App() {
+  const nav = useNavigate();
+
+  useEffect(() => {
+    nav("/");
+  }, []);
+
   return (
     <div className="App">
       <Sidebar />
